311f1a3615c656e8b30db42ffa27d3b36e37f3a0
Brennan Duck <brennantduck@gmail.com>
2024-01-10 13:16:24 -0500

    fixed success page

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ form.addEventListener("submit", (e) => {
     body: new FormData(document.querySelector("#form")),
   }).then((res) => {
     if (res.ok) {
-      window.location.href = "bduckdev.github.io/rsvp-form/success.html";
+      window.location.href =
+        "https://bduckdev.github.io/rsvp-form/success.html";
     }
   });
 });
